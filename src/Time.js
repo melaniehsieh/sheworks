75a6153155434236ddefffdb1d76f408a91e815c
melanie hsieh <mpwh2004@gmail.com>
2020-06-21 09:49:34 +0800

    added csv file

--- src/Time.js
+++ src/Time.js
@@ -3,19 +3,14 @@ import Donut from "./components/Donut";
 import Stacked from "./components/Staked";
 import "./App.css";
 
+import DonutData from "./components/csv/DonutData.csv";
+
 export default class Time extends React.Component {
   state = {
-    data: { a: 9, b: 20, c: 30, d: 8, e: 12 },
+    data: DonutData,
     width: 450,
     height: 450,
     margin: 40,
-
-    marginT: 10,
-    marginR: 30,
-    marginB: 30,
-    marginL: 60,
-    width1: 460,
-    height1: 400,
   };
 
   render() {
@@ -30,17 +25,10 @@ export default class Time extends React.Component {
             margin={this.state.margin}
           />
         </div>
-        <div>
+        {/* <div>
           <h1>Weekly</h1>
-          <Stacked
-            width={this.state.width1}
-            height={this.state.height1}
-            marginT={this.state.marginT}
-            marginR={this.state.marginR}
-            marginB={this.state.marginB}
-            marginL={this.state.marginL}
-          />
-        </div>
+          <Stacked />
+        </div> */}
       </div>
     );
   }
--- src/components/Donut.js
+++ src/components/Donut.js
@@ -22,77 +22,42 @@ class Donut extends Component {
       .append("g")
       .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-    var color = d3
-      .scaleOrdinal()
-      .domain(["a", "b", "c", "d", "e"])
-      .range(d3.schemeDark2);
+    var color = d3.scaleOrdinal().domain(data).range(d3.schemeDark2);
 
-    const pie = d3
-      .pie()
-      .sort(null)
-      .value((d) => {
-        return d.value;
-      });
+    const pie = d3.pie().value((d) => {
+      return d.value;
+    });
+
     let data_ready = pie(d3.entries(data));
 
-    const arc = d3
-      .arc()
-      .innerRadius(radius * 0.5)
-      .outerRadius(radius * 0.8);
-
-    const outerArc = d3
-      .arc()
-      .innerRadius(radius * 0.9)
-      .outerRadius(radius * 0.9);
+    const arcGenerator = d3.arc().innerRadius(0).outerRadius(radius);
 
     svg
-      .selectAll("allSlices")
+      .selectAll("mySlices")
       .data(data_ready)
       .enter()
       .append("path")
-      .attr("d", arc)
-      .attr("fill", function (d) {
+      .attr("d", arcGenerator)
+      .attr("fill", (d) => {
         return color(d.data.key);
       })
-      .attr("stroke", "white")
+      .attr("stroke", "black")
       .style("stroke-width", "2px")
       .style("opacity", 0.7);
 
     svg
-      .selectAll("allPolylines")
-      .data(data_ready)
-      .enter()
-      .append("polyline")
-      .attr("stroke", "black")
-      .style("fill", "none")
-      .attr("stroke-width", 1)
-      .attr("points", function (d) {
-        var posA = arc.centroid(d); // line insertion in the slice
-        var posB = outerArc.centroid(d); // line break: we use the other arc generator that has been built only for that
-        var posC = outerArc.centroid(d); // Label position = almost the same as posB
-        var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2; // we need the angle to see if the X position will be at the extreme right or extreme left
-        posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
-        return [posA, posB, posC];
-      });
-
-    svg
-      .selectAll("allLabels")
+      .selectAll("mySlices")
       .data(data_ready)
       .enter()
       .append("text")
-      .text(function (d) {
-        return d.data.key;
+      .text((d) => {
+        return d.data.key + d.data.timeStamp;
       })
       .attr("transform", function (d) {
-        var pos = outerArc.centroid(d);
-        var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2;
-        pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
-        return "translate(" + pos + ")";
+        return "translate(" + arcGenerator.centroid(d) + ")";
       })
-      .style("text-anchor", function (d) {
-        var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2;
-        return midangle < Math.PI ? "start" : "end";
-      });
+      .style("text-anchor", "middle")
+      .style("font-size", 17);
   }
 
   render() {
