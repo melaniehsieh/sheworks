554f59b868ac90cde6b78eddad90ff2246c65bd9
melanie hsieh <mpwh2004@gmail.com>
2020-06-21 15:30:15 +0800

    Updates

---
+++ precache-manifest.3a66369d1dd243bad632df777bd6aa8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e7fe2dd876b4994397cfdaa4541e66",
+    "url": "/sheworks/index.html"
+  },
+  {
+    "revision": "153e20258d949a4325cb",
+    "url": "/sheworks/static/css/main.88ce33be.chunk.css"
+  },
+  {
+    "revision": "cad9f56faeb44e3ed091",
+    "url": "/sheworks/static/js/2.13bb1896.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sheworks/static/js/2.13bb1896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "153e20258d949a4325cb",
+    "url": "/sheworks/static/js/main.11c235b2.chunk.js"
+  },
+  {
+    "revision": "aae83dbfa44dfc804414",
+    "url": "/sheworks/static/js/runtime-main.b9d4ecd0.js"
+  }
+]);
