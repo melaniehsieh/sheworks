ea964a0dba5d69018a6deb75a089620cb91a9286
melanie hsieh <mpwh2004@gmail.com>
2020-06-21 15:56:57 +0800

    added images

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,11 @@
 import React from "react";
-
+import img1 from "./sheworks_logo_wo_tag.png";
+import img2 from "./6.png";
+import img3 from "./7.png";
+import img4 from "./8.png";
 export const Home = () => (
   <div>
-    <img src="/sheworks_logo_wo_tag.png"></img>
+    <img src={img1}></img>
     <div id="container">
       <h1>what is sheworks?</h1>
       <p>
@@ -13,7 +16,7 @@ export const Home = () => (
     <br />
 
     <div id="container">
-      <img src="/6.png"></img>
+      <img src={img2}></img>
       <p>
         share how much time you spend on hard work with your loved ones, so they
         can help too.
@@ -21,7 +24,7 @@ export const Home = () => (
     </div>
 
     <div id="container">
-      <img src="/8.png"></img>
+      <img src={img4}></img>
       <p>
         celebrate your accomplishments with your family & friends because you
         deserve it!
@@ -29,7 +32,7 @@ export const Home = () => (
     </div>
 
     <div id="container">
-      <img src="/7.png"></img>
+      <img src={img3}></img>
       <p>
         empower yourself to appreciate your contributions, value your time, and
         do your best work.
